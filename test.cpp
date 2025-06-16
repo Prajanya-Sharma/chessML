dcc3b0acbcb38942ec5e66f4a035119d7e291059
“Prajanya-Sharma” <prajanyasharma.bt23ele@pec.edu.in>
2025-06-16 22:11:46 +0530

    test

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, this is a test program!" << endl;
+    
+    // Testing some basic operations
+    int a = 5, b = 3;
+    cout << "Sum of " << a << " and " << b << " is: " << a + b << endl;
+    
+    return 0;
+}
